b9be6c3693b2a7288de5c0bb43d982bb4d8a1642
“Danil <wanderer155-2@yandex.ru>
2021-03-14 02:27:00 +0300

    Добавьте файлы проекта.

---
+++ Laba1/Caesar.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba1
+{
+    class Caesar
+    {
+        public static string CaesarMethod(string str, uint sdv, string dir, string alph, string action)
+        {
+            int num = Convert.ToInt32(sdv);
+            char[] buffer = str.ToCharArray();
+            int max = 0;
+
+            List<char> lower = new List<char>()
+            {
+                'а' , 'б' , 'в' , 'г' , 'д' , 'е' , 'ё' , 'ж' , 'з' , 'и' , 'й' ,
+                'к' , 'л' , 'м' , 'н' , 'о' , 'п' , 'р' , 'с' , 'т' , 'у' , 'ф' ,
+                'х' , 'ц' , 'ч' , 'ш' , 'щ' , 'ъ' , 'ы' , 'ь' , 'э' , 'ю' , 'я'
+            };
+
+            List<char> upper = new List<char>()
+            {
+                'А' , 'Б' , 'В' , 'Г' , 'Д' , 'Е' , 'Ё' , 'Ж' , 'З' , 'И' , 'Й' ,
+                'К' , 'Л' , 'М' , 'Н' , 'О' , 'П' , 'Р' , 'С' , 'Т' , 'У' , 'Ф' ,
+                'Х' , 'Ц' , 'Ч' , 'Ш' , 'Щ' , 'Ъ' , 'Ы' , 'Ь' , 'Э' , 'Ю' , 'Я'
+            };
+
+            List<char> nums = new List<char>()
+            {
+                '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , '0'
+            };
+
+            if (alph == "one")
+            {
+                max = 42;
+                lower.AddRange(nums);
+                upper.AddRange(nums);
+            }
+            else
+            {
+                max = 32;
+            }
+
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                char letter = buffer[i];
+                if (upper.Contains(letter) || lower.Contains(letter) || nums.Contains(letter))
+                {
+                    if ((dir == "Right" && action == "encode") || (dir == "Left" && action == "decode"))
+                    {
+                        if (Char.IsUpper(letter) || Char.IsDigit(letter))
+                        {
+                            if (alph == "one")
+                            {
+                                if ((upper.IndexOf(letter) + num) > max)
+                                {
+                                    letter = upper[(num + upper.IndexOf(letter)) % (max + 1)];
+                                }
+                                else
+                                {
+                                    letter = upper[upper.IndexOf(letter) + num];
+                                }
+                            }
+                            else
+                            {
+                                if (Char.IsUpper(letter))
+                                {
+                                    if ((upper.IndexOf(letter) + num) > max)
+                                    {
+                                        letter = upper[(num + upper.IndexOf(letter)) % (max + 1)];
+                                    }
+                                    else
+                                    {
+                                        letter = upper[upper.IndexOf(letter) + num];
+                                    }
+                                }
+                                else
+                                {
+                                    if ((nums.IndexOf(letter) + num) > 9)
+                                    {
+                                        letter = nums[(num + nums.IndexOf(letter)) % (10)];
+                                    }
+                                    else
+                                    {
+                                        letter = nums[nums.IndexOf(letter) + num];
+                                    }
+                                }
+                            }
+                        }
+                        else if (Char.IsLower(letter) || Char.IsDigit(letter))
+                        {
+                            if (alph == "one")
+                            {
+                                if ((lower.IndexOf(letter) + num) > max)
+                                {
+                                    letter = lower[(num + lower.IndexOf(letter)) % (max + 1)];
+                                }
+                                else
+                                {
+                                    letter = lower[lower.IndexOf(letter) + num];
+                                }
+                            }
+                            else
+                            {
+                                if (Char.IsLower(letter))
+                                {
+                                    if ((lower.IndexOf(letter) + num) > max)
+                                    {
+                                        letter = lower[(num + lower.IndexOf(letter)) % (max + 1)];
+                                    }
+                                    else
+                                    {
+                                        letter = lower[lower.IndexOf(letter) + num];
+                                    }
+                                }
+                                else
+                                {
+                                    if ((nums.IndexOf(letter) + num) > 9)
+                                    {
+                                        letter = nums[(num + nums.IndexOf(letter)) % (10)];
+                                    }
+                                    else
+                                    {
+                                        letter = nums[nums.IndexOf(letter) + num];
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (Char.IsUpper(letter) || Char.IsDigit(letter))
+                        {
+                            if (alph == "one")
+                            {
+                                if ((upper.IndexOf(letter) - num) < 0)
+                                {
+                                    letter = upper[(max + 1) + (upper.IndexOf(letter) - num)];
+                                }
+                                else
+                                {
+                                    letter = upper[upper.IndexOf(letter) - num];
+                                }
+                            }
+                            else
+                            {
+                                if (Char.IsUpper(letter))
+                                {
+                                    if ((upper.IndexOf(letter) - num) < 0)
+                                    {
+                                        letter = upper[(max + 1) + (upper.IndexOf(letter) - num)];
+                                    }
+                                    else
+                                    {
+                                        letter = upper[upper.IndexOf(letter) - num];
+                                    }
+                                }
+                                else
+                                {
+                                    if ((nums.IndexOf(letter) - num) < 0)
+                                    {
+                                        letter = nums[(10) + (nums.IndexOf(letter) - num)];
+                                    }
+                                    else
+                                    {
+                                        letter = nums[nums.IndexOf(letter) - num];
+                                    }
+                                }
+                            }
+                        }
+                        else if (Char.IsLower(letter) || Char.IsDigit(letter))
+                        {
+                            if (alph == "one")
+                            {
+                                if ((lower.IndexOf(letter) - num) < 0)
+                                {
+                                    letter = lower[(max + 1) + (lower.IndexOf(letter) - num)];
+                                }
+                                else
+                                {
+                                    letter = lower[lower.IndexOf(letter) - num];
+                                }
+                            }
+                            else
+                            {
+                                if (Char.IsLower(letter))
+                                {
+                                    if ((lower.IndexOf(letter) - num) < 0)
+                                    {
+                                        letter = lower[(max + 1) + (lower.IndexOf(letter) - num)];
+                                    }
+                                    else
+                                    {
+                                        letter = lower[lower.IndexOf(letter) - num];
+                                    }
+                                }
+                                else
+                                {
+                                    if ((nums.IndexOf(letter) - num) < 0)
+                                    {
+                                        letter = nums[(10) + (nums.IndexOf(letter) - num)];
+                                    }
+                                    else
+                                    {
+                                        letter = nums[nums.IndexOf(letter) - num];
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    buffer[i] = letter;
+                }
+            }
+            return new string(buffer);
+        }
+    }
+}
---
+++ Laba1/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Laba1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+                inputTxt.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+
+        private void btnSaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, outputTxt.Text);
+        }
+
+        string direction = null;
+        string alphabet = null;
+
+        private void Dir_Checked(object sender, RoutedEventArgs e)
+        {
+            var radioButton = sender as RadioButton;
+            direction = radioButton.Name;
+        }
+
+        private void Alph_Checked(object sender, RoutedEventArgs e)
+        {
+            int length;
+            var radioButton = sender as RadioButton;
+            alphabet = radioButton.Name;
+            if (alphabet == "one")
+            {
+                length = 44;
+            }
+            else
+            {
+                length = 34;
+            }
+            for (int i = 1; i < length; i++)
+            {
+                TextBox newNote = new TextBox();
+                newNote.Text = i.ToString();
+                newNote.IsReadOnly = true;
+                Nums.Items.Add(newNote);
+            }
+        }
+
+        private void Oper_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (!String.IsNullOrEmpty(direction)&&!String.IsNullOrEmpty(alphabet)&&Nums.SelectedIndex>-1&&!String.IsNullOrWhiteSpace(inputTxt.Text))
+            {
+                MessageBox.Show("Успешно!");                
+                if (button.Name=="encode")
+                {
+                    outputTxt.Text = Caesar.CaesarMethod(inputTxt.Text, (uint)(Nums.SelectedIndex + 1), direction, alphabet, "encode");
+                }
+                else if (button.Name == "decode")
+                {
+                    outputTxt.Text = Caesar.CaesarMethod(inputTxt.Text, (uint)(Nums.SelectedIndex + 1), direction, alphabet, "decode");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Необходимо заполнить все поля!");
+            }
+        }
+    }
+}
